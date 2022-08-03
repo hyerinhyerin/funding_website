df0a85c2982a6e79072c1ef26ea0a4667d39a78b
kimjaeyoun <wodbs5602@naver.com>
2022-08-03 15:41:08 +0900

    사업자 상품 등록/수정 - 디자인 피드백 수정 ver.3

--- businessOperatorPage/index.js
+++ businessOperatorPage/index.js
@@ -13,7 +13,6 @@ function readImage(input) {
 }
 
 const inputImage = document.querySelector("#input-image");
-const plusImage = document.querySelector('#plus-image')
 
 inputImage.addEventListener("change", (e) => {
   readImage(e.target);
@@ -52,3 +51,46 @@ function uncomma(str) {
   str = String(str);
   return str.replace(/[^\d]+/g, "");
 }
+
+// 얼리버드 일반판매 부분
+function checkOnlyOne(element) {
+  const checkboxes = document.getElementsByName("status");
+
+  checkboxes.forEach((cb) => {
+    cb.checked = false;
+  });
+
+  element.checked = true;
+}
+
+var rangeDate = 31; // set limit day
+var setSdate, setEdate;
+$("#from").datepicker({
+    dateFormat: 'yy-mm-dd',
+    minDate: 0,
+    onSelect: function(selectDate){
+        var stxt = selectDate.split("-");
+            stxt[1] = stxt[1] - 1;
+        var sdate = new Date(stxt[0], stxt[1], stxt[2]);
+        var edate = new Date(stxt[0], stxt[1], stxt[2]);
+            edate.setDate(sdate.getDate() + rangeDate);
+
+        $('#to').datepicker('option', {
+            minDate: selectDate,
+            beforeShow : function () {
+                $("#to").datepicker( "option", "maxDate", edate );
+                setSdate = selectDate;
+                console.log(setSdate)
+        }});
+        //to 설정
+    }
+    //from 선택되었을 때
+});
+
+$("#to").datepicker({
+    dateFormat: 'yy-mm-dd',
+    onSelect : function(selectDate){
+        setEdate = selectDate;
+        console.log(setEdate)
+    }
+});
