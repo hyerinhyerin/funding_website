861eecb04a7a13129a13bf6198d55c74c80202dd
DBWJD\dbwjd <dbwjd20111@naver.com>
2022-08-05 16:16:58 +0900

    로그인 / 로그인, 회원가입 마무리

--- login/signup.js
+++ login/signup.js
@@ -19,5 +19,23 @@ $(document).ready(function(){
             }
         });
     });
+    $("#submit_button1").click(function(){
+        alert("사업자 회원가입이 성공되었습니다.")
+        location.href = "../Main.html";
+    })
+    $("#submit_button2").click(function(){
+        // var theForm = document.signupForm;
+
+        // if(theForm.name.value == "" || theForm.id.value == "" || theForm.password.value == "" || theForm.address.value == "" || theForm.e-mail.value == "" || theForm.phone-number.value == "" || theForm.password_answer.value == ""){
+        //     alert("모든 질문에 답을 해주세요");
+        //     return false;
+        // }else{
+        //     alert("소비자 회원가입이 성공되었습니다.");
+        //     location.href = "../Main.html";
+        // }        
+
+        alert("소비자 회원가입이 성공되었습니다.");
+        location.href = "../Main.html";
+    });
 });
 
