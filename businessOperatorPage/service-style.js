205a90fe08a1cb3676c2cb31d2b68d63fc28559f
uj0e <dhqxlajtm0408@gmail.com>
2022-08-06 17:50:48 +0900

    수정

--- businessOperatorPage/service-style.js
+++ businessOperatorPage/service-style.js
@@ -1,7 +1,13 @@
 document.getElementById("button-submit").addEventListener('click',submit);
 
+document.getElementById("buttom-category").addEventListener('click',category)
 
 function submit()
 {
-    alert("제출되었습니다.");
+    alert ("제출이 완료되었습니다.");
+}
+
+function category()
+{
+    
 }
