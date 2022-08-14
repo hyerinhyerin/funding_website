edc2cf224a01c64568a7be0624f309836673042d
uj0e <dhqxlajtm0408@gmail.com>
2022-08-15 00:27:29 +0900

    사업자-마이페이지-제작중

--- businessOperatorPage/service-style.js
+++ businessOperatorPage/service-style.js
@@ -1,12 +1,6 @@
 document.getElementById("button-submit").addEventListener('click',submit);
-document.getElementById("button-category").addEventListener('click',category)
 
 function submit()
 {
     alert("제출되었습니다.");
-}
-
-function category()
-{
-    
 }
