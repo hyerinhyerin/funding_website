0ca0c39fd77fceb46231e4f30201a8960e4695f4
jherin <jhr6454@naver.com>
2022-08-18 20:32:59 +0900

    css

--- Main.js
+++ Main.js
@@ -38,52 +38,24 @@ $(".all_menu").click(function(){
         }
     });
 });
-
-$("#mainline ul li").click(function(){
-    var _this=$(".all_menu");
-    // _this.css("color","white");
-    if(_this.css("color")=="rgb(0,0,0)"){
-        console.log("black");
-        _this.css("color","white");
-    }else{
-        console.log("white");
-        _this.css("color","black");
-    }
+$(".funding_menu").click(function(){
+    $.ajax({
+        type:'get',
+        url:'funding_plan.html',
+        dataType:'html',
+        success:function(data){
+            $("#item-list").html(data);
+        }
+    });
 });
-// $(".listsort.new").click(function(){
-//     $(".progress-done").each(function(){
-//         var num=$(this).attr("data-done");
-//         for(var i=0;i<num.length;i++){
-//             if(num[i]>num[i+1]){
-//                 var number=num[i];
-//                 num[i]=num[i+1];
-//                 num[i+1]=number;
-//             }
-//         }
-//         for(var k=0;k<num.length;i++){
-//             console.log(num[k]);
-//         }
-//     });
-// });
-// $(".listsort.best").click(function(){
-//     $(".progress-done").each(function(){
-//         var items=$(this).attr('data-done');
-//         for(var i=0;i<items.length;i++){
-//             if(items[i]<items[i+1]){
-//                 var item=items[i];
-//                 items[i]=items[i+1];
-//                 items[i+1]=item;
-//             }
-//         }
-//         console.log(items);
-//         // for(var i=0;i<items.length;i++){
-//         //     if(items[i]==$(".item .progress-done").attr('data-done')){
-//         //         var thisitem=items[i].parent("div.item");
-//         //         console.log(thisitem);
-//         //     }
-//         // }
-        
-//         // console.log(items);
-//     });
-    
-// })
+
+$(".earlybird_menu").click(function(){
+    $.ajax({
+        type:'get',
+        url:'earlybird.html',
+        dataType:'html',
+        success:function(data){
+            $("#item-list").html(data);
+        }
+    });
+});
